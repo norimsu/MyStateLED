4a8a481e6e0ff7ddb4c523fe1cdfa0855ae0c349
SANGHYUN <smarthyun88@lotte.net>
2018-09-19 12:40:52 +0900

    gpio test

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-var 
+var Gpio = require('onoff').Gpio;
+
+var led = new Gpio(21, 'out');
+
+var led_state = 0;
+
+var iv = setInterval(function() {
+    led_state = led.readSync();
+    if (led_state == 0) led_state = 1;
+    else led_state = 0;
+    led.writeSync(led_state)
+}, 200);
+
+setTimeout(function() {
+    clearInterval(iv);
+    led.writeSync(0);
+    led.unexport();
+}, 5000);
